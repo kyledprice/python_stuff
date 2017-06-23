375d86476df662216b16b1e87f69ed9edb2137a7
Kyle Price <kyle.price@plexus.com>
2017-06-23 15:04:12 -0600

    mostly working, just a few bugs left

--- SortedSet/BinarySearchSet.py
+++ SortedSet/BinarySearchSet.py
@@ -9,37 +9,78 @@ class BinarySearchSet:
     def last(self):
         return self.base_array[len(self.base_array) - 1]
 
+    # Shift the values in the array to the right
     def shift_right(self, index):
-        for i in range(len(self.base_array) - 1, index):
-            self.base_array[i + 1] = self.base_array[i]
+        if len(self.base_array) - 1 == index:
+            self.base_array.append(self.base_array[index])
+            return
+        for i in range(len(self.base_array) - 1, index - 1, -1):
+            if i == len(self.base_array) - 1:
+                self.base_array.append(self.base_array[i])
+            else:
+                self.base_array[i + 1] = self.base_array[i]
 
     def add(self, item):
+        # If the array was empty
         if len(self.base_array) == 0:
             self.base_array.append(item)
         else:
             low_index = 0
             high_index = len(self.base_array) - 1
+            # When the indices cross, you have the right index
             while low_index < high_index:
-                middle = (high_index + low_index) / 2
+                middle = low_index + (high_index - low_index) / 2
+                # If the item is found, return false
                 if item == self.base_array[middle]:
                     return False
                 else:
                     if item < self.base_array[middle]:
-                        high_index /= 2 + 1
+                        high_index = middle - 1
                         continue
                     else:
                         if item > self.base_array[middle]:
-                            low_index += (high_index / 2) + 1
+                            low_index = middle + 1
                             continue
-            self.shift_right(low_index + high_index / 2)
-            self[low_index + high_index / 2]  = item
+            # Handle the case where the array size is 1
+            if len(self.base_array) == 1:
+                if self.base_array[0] == item:
+                    return False
+                else:
+                    if self.base_array[0] < item:
+                        self.base_array.append(item)
+                        return True
+                    else:
+                        self.base_array[1] = self.base_array[0]
+                        self.base_array[0] = item
+                        return True
+            # Check to see if when the indices crossed, the correct value was found
+            if self.base_array[high_index] == item:
+                return False
+            # Shift the values to the right
+            print(high_index)
+            self.shift_right(high_index)
+            # Insert the value
+            self.base_array[high_index] = item
             return True
 
 
-
 bss = BinarySearchSet()
-bss.base_array.append(4)
-bss.base_array.append(5)
-bss.base_array.append(7)
-print(bss.first())
-print(bss.last())
+bss.add(5)
+print(bss.base_array)
+bss.add(7)
+print(bss.base_array)
+bss.add(6)
+print(bss.base_array)
+bss.add(7)
+print(bss.base_array)
+
+bss.add(-12)
+print(bss.base_array)
+bss.add(-12)
+print(bss.base_array)
+bss.add(0)
+print(bss.base_array)
+bss.add(123)
+print(bss.base_array)
+
+
